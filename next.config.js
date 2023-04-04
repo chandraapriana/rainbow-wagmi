a7f1e0df8fb39864cd0fd2dfca64c043eb6cf299
Chandra Muhamad Apriana <aprianachandra@gmail.com>
2023-04-04 22:47:59 +0700

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    formats: ["image/avif", "image/webp"],
+    domains: [
+      "storage.googleapis.com",
+      "i.seadn.io",
+      "res.cloudinary.com",
+      "nft-cdn.alchemy.com",
+      "ipfs.io",
+      "kometh.mypinata.cloud",
+      "gaspack.mypinata.cloud",
+      "gateway.pinata.cloud",
+      "upload.wikimedia.org",
+    ],
+    minimumCacheTTL: 1500000,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,111 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-import { Container } from "@chakra-ui/react";
+import { Container, Text, Box, Button } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import abiFile from "./../contracts/abiFile.json";
+import {
+  useContractRead,
+  useAccount,
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+
+const CONTRACT_ADDRESS = "0xaa3906f986e0cd86e64c1e30ce500c1de1ef46ad";
 export default function Home() {
+  const { address } = useAccount();
+  const [img, setImg] = useState("");
+  const {
+    data: dataRead,
+    isError: isErrorRead,
+    isLoading: isLoadingRead,
+  } = useContractRead({
+    address: CONTRACT_ADDRESS,
+    abi: abiFile,
+    functionName: "commonTokenURI",
+  });
+  const { data: dataMinted } = useContractRead({
+    address: CONTRACT_ADDRESS,
+    abi: abiFile,
+    functionName: "balanceOf",
+    args: [address],
+  });
+  const {
+    config,
+    error: prepareError,
+    isError: isPrepareError,
+  } = usePrepareContractWrite({
+    address: CONTRACT_ADDRESS,
+    abi: abiFile,
+    functionName: "mint",
+    args: [
+      address,
+      {
+        value: ethers.utils.parseEther("0.001"),
+      },
+    ],
+  });
+  const { data, error, isError, write } = useContractWrite(config);
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+  useEffect(() => {
+    (async () => {
+      if (dataRead) {
+        const res = await (await fetch(dataRead as unknown as string)).json();
+        setImg(res.image);
+      }
+    })();
+  }, [dataRead]);
+
+  useEffect(() => {
+    console.log(dataMinted);
+  }, [dataMinted]);
+
   return (
     <Container>
       <ConnectButton />
+      <Text>This is the NFT we will be minting!</Text>
+      <Box
+        as={motion.div}
+        borderColor="gray.200"
+        borderWidth="1px"
+        width="fit-content"
+        marginTop="4"
+        padding="6"
+        shadow="md"
+        rounded="lg"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}>
+        <Image alt="nft image" width={200} height={200} src={img}></Image>
+      </Box>
+      <div>
+        <Button isDisabled={!write || isLoading} onClick={write}>
+          {isLoading ? "Minting..." : "Mint"}
+        </Button>
+        {isSuccess && (
+          <div>
+            Successfully minted your NFT!
+            <div>
+              <a href={`https://goerli.etherscan.io/tx/${data?.hash}`}>
+                Etherscan
+              </a>
+            </div>
+          </div>
+        )}
+        {(isPrepareError || isError) && (
+          <div>Error: {(prepareError || error)?.message}</div>
+        )}
+        {dataMinted && (
+          <Text>You Have Minted : {parseInt(dataMinted, 16)}</Text>
+        )}
+      </div>
     </Container>
   );
 }
